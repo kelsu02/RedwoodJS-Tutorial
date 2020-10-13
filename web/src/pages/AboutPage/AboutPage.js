75439f7a4c5cfc5eb2dcd1dbe1625b8ffd98ccfb
kredmond <kelsu02@gmail.com>
2020-10-12 19:09:04 -0600

    getting ready to deploy

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>This was created to show off Redwood.</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
